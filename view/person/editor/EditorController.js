de04344f585ec038185eacd934cbf439339206cd
glenr4 <glenrutherford4@gmail.com>
2016-05-02 15:40:35 +1000

    Syncs to store

--- view/person/editor/EditorController.js
+++ view/person/editor/EditorController.js
@@ -34,7 +34,7 @@
         console.log(record.dirty);
 
         if (record.dirty) { // must use this test with data binding
-        //if (form.isDirty()) {
+            //if (form.isDirty()) {
             Ext.Msg.show({
                 title: 'Save Changes?',
                 message: 'You are closing a tab that has unsaved changes. Would you like to save your changes?',
@@ -43,7 +43,8 @@
                 fn: function (btn) {
                     if (btn === 'yes') {
                         console.log('Yes pressed');
-                        
+
+                        self.syncStore(store);
                         self.loadPreviousTab();
                     } else if (btn === 'no') {
                         console.log('No pressed');
@@ -58,11 +59,29 @@
             });
         } else {
             self.loadPreviousTab();
-        }
+        };
+    },
+    syncStore: function (store) {
+        console.log('EditorController: syncStore');
+        store.sync({
+            success: function (e) {
+                console.log("syncSuccess");
+                console.log(e);
+            },
+            failure: function (e) {
+                console.log("syncFailure");
+                console.log(e);
+            },
+            callback: function () {
+                console.log("callback")
+            }
+        });
     },
     onSave: function () {
         console.log("EditorController: onSave");
-           
+
+        var store = Ext.data.StoreManager.lookup('personstore');
+        this.syncStore(store);
         this.loadPreviousTab();
     },
     loadPreviousTab: function () {
